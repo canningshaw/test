a59f03d4b53ca0688d95b2e36e541195bdc5d51f
canningshaw <cainingxiao@hotmail.com>
2015-06-10 21:12:22 +0800

    python datetime

--- helloworld/helloworld/helloworld.py
+++ helloworld/helloworld/helloworld.py
@@ -1,5 +1,10 @@
+import datetime
+currentdate = datetime.datetime.now()
+print(currentdate)
 
-num =input("what is your Name?")
-
-print(num)
-
+##print(currentdate.strftime('%d %b,%Y'))
+#userinput = input('please enter your birthday')
+#birthday =datetime.datetime.strptime(str(userinput),'%m%d%Y').date()
+#print(birthday)
+#haha = currentdate - birthday
+#print(haha)
